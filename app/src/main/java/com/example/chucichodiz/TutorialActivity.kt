4b819f849c2789a05d69c7b372e097d9c606a355
Kacper Młynarski <kacpermln@wp.pl>
2022-09-17 21:13:10 +0200

    Update TutorialActivity.kt

--- app/src/main/java/com/example/chucichodiz/TutorialActivity.kt
+++ app/src/main/java/com/example/chucichodiz/TutorialActivity.kt
@@ -6,7 +6,11 @@ import android.os.Bundle
 class TutorialActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_meme)
+        setContentView(R.layout.activity_tutorial)
+
+        val actionBar = supportActionBar // helpful var
+        actionBar!!.title = "Poradnik" // changing title of activity
+        actionBar.setDisplayHomeAsUpEnabled(true) // displaying back button
     }
 
     override fun onSupportNavigateUp(): Boolean {
