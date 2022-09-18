16d5086587dc341a71c1316bbad4e6b18978b742
Kacper Młynarski <kacpermln@wp.pl>
2022-09-18 21:25:20 +0200

    Update TutorialActivity.kt

--- app/src/main/java/com/example/chucichodiz/TutorialActivity.kt
+++ app/src/main/java/com/example/chucichodiz/TutorialActivity.kt
@@ -1,15 +1,16 @@
 package com.example.chucichodiz
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class TutorialActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tutorial)
+        setContentView(R.layout.activity_calendar)
 
         val actionBar = supportActionBar // helpful var
-        actionBar!!.title = "Poradnik" // changing title of activity
+        actionBar!!.title = "Kalendarz" // changing title of activity
         actionBar.setDisplayHomeAsUpEnabled(true) // displaying back button
     }
 
