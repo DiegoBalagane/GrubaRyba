575f290fc6dc9a93d57e78da4b25a6c98d54dc1f
Kacper Młynarski <kacpermln@wp.pl>
2022-09-18 21:25:18 +0200

    Create CalendarActivity.kt

---
+++ app/src/main/java/com/example/chucichodiz/CalendarActivity.kt
@@ -0,0 +1,20 @@
+package com.example.chucichodiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CalendarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar)
+
+        val actionBar = supportActionBar // helpful var
+        actionBar!!.title = "Kalendarz" // changing title of activity
+        actionBar.setDisplayHomeAsUpEnabled(true) // displaying back button
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+}
